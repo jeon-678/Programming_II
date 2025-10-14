17f0c5b81405965354277c1f22e33fbf50f980ba
jeon-678 <jooon28412@gmail.com>
2025-10-15 06:37:00 +0900

    asd

--- Chapter_07/Program_Assignment/PA02*.c
+++ Chapter_07/Program_Assignment/PA02*.c
@@ -8,3 +8,50 @@
 공비? [ 5 ]
 등비수열: 1.2 6 30 150 750 3750 18750 93750 468750 2.34375e+06
 */
+
+#include <stdio.h>
+#include <math.h>
+#define SIZE 10
+
+double input();
+double common_ratio(double a, double r, int i);
+
+int main() {
+	input();
+
+	return 0;
+}
+/*
+* 함수명: input
+* 기능: 사용자에게 첫 항과 공비를 입력받는다.
+*/
+double input() {
+	double a, r, arr[SIZE] = { 0 };
+	int i = 0;
+
+	printf("첫 번째 항? ");
+	scanf("%lf", &a);
+
+	printf("공비? ");
+	scanf("%lf", &r);
+
+	printf("등비수열: ");
+
+	while (i < SIZE) {
+		arr[i] = common_ratio(a, r, i);
+
+		i++;
+	}
+
+	for (int j = 0; j < SIZE; j++) {
+		printf("%g ", arr[j]);
+	}
+}
+/*
+* 함수명: common_ratio
+* 기능: 전달받은 인자값을 이용하여 등비수열의 결과를 계산한다.
+* 반환: 계산된 수열의 값
+*/
+double common_ratio(double a, double r, int i) {
+	return a * pow(r, i);
+}
--- Chapter_07/Program_Assignment/PA04**.c
+++ Chapter_07/Program_Assignment/PA04**.c
@@ -7,3 +7,37 @@
 최대값: 인덱스=4 값=99
 최소값: 인덱스=12 값=12
 */
+
+#include <stdio.h>
+#define SIZE 10
+
+int ARR();
+
+int main() {
+
+	ARR();
+
+	return 0;
+}
+
+int ARR() {
+	int arr[SIZE] = { 23, 45 ,62, 12, 99, 56, 87, 32, 54, 13 }, max, min, index_max, index_min;
+
+	printf("배열? ");
+	for (int i = 0; i < SIZE; i++) {
+		printf("%d ", arr[i]);
+	}
+	max = min = arr[0];
+
+	for (int i = 0; i < SIZE; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+			index_max = i;
+		}
+		else if (min > arr[i]) {
+			min = arr[i];
+			index_min = i;
+		}
+	}
+	printf("\n최댓값: 인덱스 = %d, 값 = %d\n최솟값: 인덱스 = %d, 값 = %d", index_max, max, index_min, min);
+}
