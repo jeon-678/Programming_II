150f64a22e264184ff99df3353534da3ee873bc0
jeon-678 <jooon28412@gmail.com>
2025-11-04 14:50:55 +0900

    11.4

---
+++ Chapter_12/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int test_io(void);
+
+int main(int argc, char* argv[]){
+    
+    printf("Hello World!\n");
+
+    fprintf(stdout, "Hello World!\n");
+    
+    fprintf(stderr, "Hello World!\n");
+    
+    test_io();
+
+    return 0;
+}
+
+#define FNAME "BTS.txt"
+
+int test_io(void){
+
+    int x = 0;
+
+    int res = fscanf(stdin, "%d", &x);
+    fprintf(stdout, "입력값: %d, 반환값: %d\n", x, res);
+
+    FILE * fBTS = NULL;
+
+    fBTS = fopen("BTS.txt", "w");
+    if(fBTS == NULL){
+        fprintf(stderr, "파일을 열 수 없습니다.\n");
+        // exit(0);
+    }
+    else{
+        int res = fscanf(stdin, "%d", &x);
+        fprintf(stdout, "입력값: %d, 반환값: %d\n", x, res);
+        fprintf(fBTS, "입력값: %d, 반환값: %d\n", x, res);
+    }
+
+    fflush(fBTS);
+    fclose(fBTS);
+
+    return 0;
+}
