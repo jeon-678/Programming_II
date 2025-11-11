f34336cef4c1d30233a7101b9b75aa25e184c549
jeon-678 <jooon28412@gmail.com>
2025-11-11 14:40:56 +0900

    htf

--- Chapter_13/Class/main.c
+++ Chapter_13/Class/main.c
@@ -1,6 +1,13 @@
+// debug mode, release mode
+
 #include <stdio.h>
+#include "point.h"
 
 int main(){
+
+    POINT k = {3, 4};
+
+    printf("%d %d\n", k.x, k.y);
 
     return 0;
 }
---
+++ Chapter_13/Class/point.h
@@ -0,0 +1,14 @@
+#ifndef _POINT_H_293844322
+#define _POINT_H_293844322
+
+typedef struct _point{
+    int x;
+    int y;
+} POINT;
+
+
+
+
+
+
+#endif // _POINT_H_293844322
